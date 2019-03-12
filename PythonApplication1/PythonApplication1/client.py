db5bca9741533df9faba283fad73b4b021387853
Brian Novius <v.brianvis@icloud.com>
2019-03-12 09:38:40 +0100

    aa

---
+++ PythonApplication1/PythonApplication1/client.py
@@ -0,0 +1,31 @@
+import socket
+import threading
+LHOST = '127.0.0.1'
+LPORT = 80
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.connect((LHOST, LPORT))
+
+def one():
+    print("start1")
+    while True:
+        try:
+            inp = input()
+            s.send(inp.encode())
+        except Exception as e:
+            print(str(e))
+
+def two():
+    print("start")
+    while True:
+        q = s.recv(4096).decode()
+        print(q)
+
+t1 = threading.Thread(target = one)
+t2 = threading.Thread(target = two)
+t1.setDaemon(True)
+t2.setDaemon(True)
+t1.start()
+t2.start()
+while True:
+    pass
